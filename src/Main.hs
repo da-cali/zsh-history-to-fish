c8e42dde4ea6074b3567845ceeaf41fdf5b05666
Dan <dan@localhost.localdomain>
2019-01-10 19:19:50 -0800

    success message added

--- src/Main.hs
+++ src/Main.hs
@@ -13,13 +13,13 @@ getDates (x:xs)
   | otherwise = getDates xs
 
 main :: IO ()
-main = do  
-    zshHistory <- readFile ".zsh_history"  
-    let commandLines = map ("- cmd: " ++) (getCommands (lines zshHistory))
-        dateLines = map ("  when: " ++) (getDates (lines zshHistory))
-        fishHistory = combine commandLines dateLines where
-          combine [] _ = []
-          combine _ [] = []
-          combine (c:cs) (d:ds) = c : d : combine cs ds
-    writeFile "fish_history" (unlines fishHistory)
-    
+main = do
+  zshHistory <- readFile ".zsh_history"  
+  let fishHistory = combine commandLines dateLines where
+        commandLines = map ("- cmd: " ++) (getCommands $ lines zshHistory)
+        dateLines = map ("  when: " ++) (getDates $ lines zshHistory)
+        combine [] _ = []
+        combine _ [] = []
+        combine (c:cs) (d:ds) = c : d : combine cs ds
+  writeFile "fish_history" (unlines fishHistory)
+  putStrLn "fish_history file created."
